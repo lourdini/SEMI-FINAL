274d4653ab6e63f1d493dc8208c833fb02d03330
Lourd Anthony Virtudazo <lourdanthony17@gmail.com>
2025-05-08 09:48:14 +0800

    Third Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Lourd Anthony Virtudazo</h1>'
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
