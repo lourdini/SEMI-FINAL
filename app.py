c0a6592cf319fe7d8787c16de542a694c07cb945
Lourd Anthony Virtudazo <lourdanthony17@gmail.com>
2025-05-08 09:53:51 +0800

    Fourth Commit

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Lourd Anthony Virtudazo</h1>'
+    return ('<p>Lourd Anthony Virtudazo'
+            'BSIT-III'
+            'System Integration'
+            'Semi-Final Exam</p>')
 
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port)
